7688e8e836cf24e7dd7abb7dfc5b4dca55d32822
masweetman <slosweetman@gmail.com>
2015-08-31 21:50:55 -0700

    improved query

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,6 +2,6 @@ module ApplicationHelper
   def sortable(column, title = nil)
     title ||= column.titleize
     direction = (column == params[:sort] && params[:direction] == "asc") ? "desc" : "asc"
-    link_to title, :sort => column, :direction => direction
+    link_to title, params.merge(:sort => column, :direction => direction)
   end
 end
